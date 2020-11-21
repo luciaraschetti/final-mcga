fd755804e5a11fdc40bd5eb0a9c7b54835a8cffe
lucia raschetti <lucia.sn011@gmail.com>
2020-11-21 17:04:08 -0300

    Define routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import userRoutes from './routes/user.routes';
+import gameRoutes from './routes/game.routes';
+import defaultRulesRoutes from './routes/defaultRule.routes';
+import gameRatingRoutes from './routes/gameRating.routes';
+import tournamentRoutes from './routes/tournament.routes';
+import roleRoutes from './routes/Role.routes';
 
 const app = express();
-
 
 app.set('port',3000);
 
@@ -12,5 +17,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.use(userRoutes);
+app.use(gameRoutes);
+app.use(defaultRulesRoutes);
+app.use(gameRatingRoutes);
+app.use(tournamentRoutes);
+app.use(roleRoutes);
 
 export default app;

b103f46801386935b9e8af9ea2684519ecad1e0a
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2020-11-22 14:49:43 -0300

    Add react router and routes

---
+++ frontend/src/componets/screens/Login/Login.js
@@ -0,0 +1,10 @@
+import './styles.css';
+import React from 'react';
+
+class Login extends React.Component{
+    render() {
+        return <h1>Hola soy el login</h1>
+    }
+}
+
+export default Login;
---
+++ frontend/src/componets/screens/Register/Register.js
@@ -0,0 +1,10 @@
+import './styles.css';
+import React from 'react';
+
+class Login extends React.Component{
+    render() {
+        return <h2>Hola soy el registro</h2>
+    }
+}
+
+export default Login;

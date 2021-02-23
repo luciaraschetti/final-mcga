daf2791e5c507478ab7d4ff79016191e2aed7d7c
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2021-02-23 11:58:09 -0300

    Modify Login and Add new error alert

--- frontend/src/components/screens/Login/index.tsx
+++ frontend/src/components/screens/Login/index.tsx
@@ -1,11 +1,21 @@
+import './style.css';
 import React, { useState } from 'react';
 import Layout from '../../layouts/Login/index'
 import axios, { AxiosResponse } from 'axios';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Logo from '../../layouts/Public/Navbar/img/pngegg.png';
+import swal from 'sweetalert';
+
 
 const Index = () => {
 
 	const [email, setEmail] = useState<string>("")
 	const [password, setPassword] = useState<string>("")
+
+	const error = () => swal("User invalid!", "Try again or Register Now!!!");
+
+
 
 	const login = () => {
 		axios.post("http://localhost:4000/api/auth/signin", {
@@ -19,15 +29,33 @@ const Index = () => {
 			// }
 		}, () => {
 			console.log("Failure");
+			error()
+
 		})
 	}
 
 	return (
 		<Layout buttonText='Register'>
 			<div className="login">
-				<input type="text" placeholder='username' onChange={e => setEmail(e.target.value)} />
-				<input type="text" placeholder='password' onChange={e => setPassword(e.target.value)} />
-				<button onClick={login}>Login</button>
+				<img src={Logo} className="logoNavbar" />
+				<div className="loginForm">
+					<TextField id="outlined-basic"
+						variant="outlined"
+						placeholder='Username'
+						onChange={e => setEmail(e.target.value)}
+						fullWidth
+					/>
+					<TextField id="outlined-basic"
+						variant="outlined"
+						type="password"
+						placeholder='Password'
+						onChange={e => setPassword(e.target.value)}
+						fullWidth
+					/>
+					<Button variant="contained" color="primary" onClick={login}>
+						Login
+      				</Button>
+				</div>
 			</div>
 		</Layout>
 	);
